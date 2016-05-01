83edb6ca385a5b4f0a3b171a1cdd636a2f136be7
A Man <abbasfreestyle@gmail.com>
2016-05-01 20:40:03 +0100

    Update VitamioViewManager.java

--- src/main/java/com/RNVitamioView/VitamioViewManager.java
+++ src/main/java/com/RNVitamioView/VitamioViewManager.java
@@ -8,7 +8,7 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.BaseViewPropertyApplicator;
 import com.facebook.react.uimanager.CatalystStylesDiffMap;
 import com.facebook.react.uimanager.SimpleViewManager;
-import com.facebook.react.uimanager.annotations.ThemedReactContext;
+import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.ReactProp;
 import com.facebook.react.uimanager.ViewManager;
 import com.facebook.react.views.text.ReactTextShadowNode;
